78c119c2c0f98b7bf1218c415f910494a3aa5c92
andresperezfirigua <andresperezfirigua@gmail.com>
2024-03-18 12:40:52 -0500

    Completed script

--- Form_Filling/form_filling_script.py
+++ Form_Filling/form_filling_script.py
@@ -1,30 +1,36 @@
 import csv
-from docxtpl import DocxTemplate, InlineImage
-from docx.shared import Inches
-import comtypes.client
+from docxtpl import DocxTemplate
+import datetime
 
 # Load the .docx template
-doc = DocxTemplate("template.docx")
+doc = DocxTemplate("Asignation_Template.docx")
 
 # Open the .csv file
 with open("data.csv", "r") as csvfile:
-    reader = csv.DictReader(csvfile)
+    reader = csv.DictReader(csvfile, delimiter=';')
 
     # Iterate through each row in the .csv file
     for row in reader:
         # Create a context dictionary with the data from the .csv row
         context = row
 
+        # Edit LOB from data
+        context['lob'] = context['lob'].split(' - ')[1]
+
+        # Edit serial number
+        context['serial_number'] = f'00{context["serial_number"]}'
+
+        # Add current date
+        date = datetime.datetime.now()
+        context['date'] = date.strftime('%B %d, %Y')
+
         # Render the .docx template with the data from the .csv row
-        doc.render(context)
+        doc.render(context, autoescape=True)
 
         # Save the filled form as a .docx file
-        doc.save(f"{row['name']}.docx")
+        file_name = f"{row['emp_eeid']} - {row['emp_name']}"
+        doc.save(f"Filled_Forms/Linda_Rozo/{file_name}.docx")
 
-        # Convert the .docx file to a .pdf file
-        word = comtypes.client.CreateObject("Word.Application")
-        doc = word.Documents.Open(f"{row['name']}.docx")
-        doc.SaveAs(f"{row['name']}.pdf", FileFormat=17)
-        doc.Close()
+        print(f"Filled_Forms/Linda_Rozo/{file_name}.docx")
 
-    word.Quit()
+    # word.Quit()
